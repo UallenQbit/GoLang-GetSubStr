a841c151cb20695d746b5a068b974d7fef019c37
UallenQbit <72503738+UallenQbit@users.noreply.github.com>
2022-06-03 21:36:01 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package GetSubStr
+
+import "strings"
+
+func GetSubStr(String string, LeftString string, RightString string) (SubString string) {
+	if LeftIndex := strings.Index(String, LeftString); LeftIndex == -1 {
+		return ""
+	} else {
+		RightSubString := String[LeftIndex:]
+		if RightIndex := strings.Index(RightSubString, RightString); RightIndex == -1 {
+			return ""
+		} else {
+			return RightSubString[:RightIndex]
+		}
+	}
+}
